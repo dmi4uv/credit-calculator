e53d0957cb802108e81f9062683ced1c609df1ba
Chuvykin Dmitry <dmi4uv@gmail.com>
2020-10-27 02:40:39 +0400

    Updates

---
+++ precache-manifest.e268fd892edadfc9db507ebcd0c095b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d6eb72ce0d68b44038e0daed586c31c",
+    "url": "/credit-calculator/index.html"
+  },
+  {
+    "revision": "203dee680ad693176b37",
+    "url": "/credit-calculator/static/css/main.79e2ef5b.chunk.css"
+  },
+  {
+    "revision": "75d2806c328b2cf7c10d",
+    "url": "/credit-calculator/static/js/2.c93ea24c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/credit-calculator/static/js/2.c93ea24c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "203dee680ad693176b37",
+    "url": "/credit-calculator/static/js/main.e381372b.chunk.js"
+  },
+  {
+    "revision": "2f57ffb70b62762f08a1",
+    "url": "/credit-calculator/static/js/runtime-main.db1a3a05.js"
+  }
+]);
